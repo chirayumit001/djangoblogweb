25d8a56f968af04aadc340579925edafbc4d115f
chirayumit001 <chirayumit001@gmail.com>
2021-11-01 12:15:52 +0530

    Made blog name

--- djangonautic/settings.py
+++ djangonautic/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-xohmpzcmzid)-pzidx+@fyc%&7t&7)_qps9lgsf5d-7-#mkii3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'blogchirayu.herokuapp.com']
 
 
 # Application definition
